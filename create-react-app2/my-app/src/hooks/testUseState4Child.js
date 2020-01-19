fee7ed657f1382ae02e07ac641ad86d4c1f2dbc4
chenqiliang <chenqiliang@qtrade.com.cn>
2020-01-19 17:12:09 +0800

    feat: setTimeout in useState

--- create-react-app2/my-app/src/hooks/testUseState4Child.js
+++ create-react-app2/my-app/src/hooks/testUseState4Child.js
@@ -10,7 +10,7 @@ return (<span>usMoney: {props.money.usMoney}</span>)
 }, (prevProps, nextProps)=>{
   console.log('prev:' + JSON.stringify(prevProps.money, null, 4));
   console.log('next:' + JSON.stringify(nextProps.money, null, 4));
-  return isEqual(prevProps.money.usMoney, nextProps.money.usMoney);
+  return isEqual(prevProps.money, nextProps.money);
 });
 
 // const Child = React.memo((props)=>{
@@ -24,19 +24,27 @@ return (<span>usMoney: {props.money.usMoney}</span>)
 export default function TestUseState4Child() {
   const [money, setMoney] = useState({usMoney: 5});
 
-  // const [ count, setCount ] = useState(1);
+  // const handleMoney = () => {    
+  //   const newUsMoney = money.usMoney + 1;
+  //   setMoney({
+  //     ...money, usMoney: newUsMoney
+  //   })
+  //   // setMoney(money=>{
+  //   //   money.usMoney = money.usMoney+1;
+  //   //   const newMone = cloneDeep(money);
+  //   //   console.log(newMone);
+  //   //   return money;
+  //   // });
+  // }
 
-  const handleMoney = () => {    
-    const newUsMoney = money.usMoney + 1;
-    setMoney({
-      ...money, usMoney: newUsMoney
-    })
-    // setMoney(money=>{
-    //   money.usMoney = money.usMoney+1;
-    //   const newMone = cloneDeep(money);
-    //   console.log(newMone);
-    //   return money;
-    // });
+  const handleMoney = () => { 
+    setTimeout(()=>{
+      // const newUsMoney = money.usMoney + 1;
+      // setMoney({
+      //   ...money, usMoney: newUsMoney
+      // })
+      setMoney({usMoney: money.usMoney+1})
+    },1000); 
   }
 
   console.log('parent called');
