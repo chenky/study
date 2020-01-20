043760b62dbdc28b49928737cf856b6c5b2d103d
chenqiliang <chenqiliang@qtrade.com.cn>
2020-01-20 19:14:13 +0800

    hooks实例代码全部移动到一个文件下

--- create-react-app2/my-app/src/App.js
+++ create-react-app2/my-app/src/App.js
@@ -1,63 +1,12 @@
-import React, {useState} from 'react';
-import { Button } from 'antd';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-// import StudyHook from './hooks/index';
-// import UseDocTitle from './hooks/useDocTitle'
-// import TestUseDocTitle from './hooks/testUseDocTitle';
-// import TestUseEffect from './hooks/testUseEffect'
-// import TestUseContext from './hooks/testUseContext';
-// import TestUseState from './hooks/testUseState';
-// import TestUseReducer from './hooks/testUseReducer';
-// import TestUseCallback from './hooks/testUseCallback';
-// import TestUseImperativeHandle from './hooks/testUseImperativeHandle';
-// import TestMemo from './hooks/testMemo';
-// import TestUseState4Child from './hooks/testUseState4Child';
-// import Counter from './hooks/hook_detail/count'
-// import Counter2 from './hooks/hook_detail/memo';
-// import Effect from './hooks/hook_detail/effect';
-// import LayoutEffect from './hooks/hook_detail/layoutEffect';
-// import ForwardRef from './hooks/hook_detail/forwardRef';
-// import { Counter1, Counter2 } from './hooks/hook_detail/customHook';
-// import CaptureValue from './hooks/hook_detail2/capture_value';
-// import RenderProps from './hooks/hook_detail2/renderProps';
-// import RenderProps2 from './hooks/hook_detail2/renderProps2';
-// import TestUseResize from './hooks/hook_detail2/useResize';
-import TestUseEffect from './hooks/hook_detail2/TestUseEffect';
+import HooksIndex from './hooks/index';
+
 
 function App() {
-  // let [ show, setShow ] = useState(true);
-
   return (
     <div className="App">
-      {/* <StudyHook></StudyHook> */}
-      {/* <Button onClick={()=>{ setShow(!show) }}>show</Button>
-      {
-        show ? <TestUseDocTitle></TestUseDocTitle> : null
-      } */}
-      {/* <TestUseDocTitle></TestUseDocTitle> */}
-      {/* <TestUseEffect></TestUseEffect> */}
-      {/* <TestUseContext></TestUseContext> */}
-      {/* <TestUseState></TestUseState> */}
-      {/* <TestUseReducer></TestUseReducer> */}
-      {/* <TestUseCallback></TestUseCallback> */}
-      {/* <TestUseImperativeHandle></TestUseImperativeHandle> */}
-      {/* <TestMemo></TestMemo> */}
-      {/* <TestUseState4Child></TestUseState4Child> */}
-      {/* <Counter></Counter> */}
-      {/* <Counter2></Counter2> */}
-      {/* <Effect></Effect> */}
-      {/* <LayoutEffect></LayoutEffect> */}
-      {/* <ForwardRef></ForwardRef> */}
-      {/* <div>
-        <Counter1></Counter1>
-        <Counter2></Counter2>
-      </div> */}
-      {/* <CaptureValue></CaptureValue> */}
-      {/* <RenderProps></RenderProps> */}
-      {/* <RenderProps2></RenderProps2> */}
-      {/* <TestUseResize></TestUseResize> */}
-      <TestUseEffect></TestUseEffect>
+      <HooksIndex></HooksIndex>
     </div>
   );
 }
---
+++ create-react-app2/my-app/src/hooks/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+// import StudyHook from './index';
+// import UseDocTitle from './useDocTitle'
+// import TestUseDocTitle from './testUseDocTitle';
+// import TestUseEffect from './testUseEffect'
+// import TestUseContext from './testUseContext';
+// import TestUseState from './testUseState';
+// import TestUseReducer from './testUseReducer';
+// import TestUseCallback from './testUseCallback';
+// import TestUseImperativeHandle from './testUseImperativeHandle';
+// import TestMemo from './testMemo';
+// import TestUseState4Child from './testUseState4Child';
+// import Counter from './hook_detail/count'
+// import Counter2 from './hook_detail/memo';
+// import Effect from './hook_detail/effect';
+// import LayoutEffect from './hook_detail/layoutEffect';
+// import ForwardRef from './hook_detail/forwardRef';
+// import { Counter1, Counter2 } from './hook_detail/customHook';
+// import CaptureValue from './hook_detail2/capture_value';
+// import RenderProps from './hook_detail2/renderProps';
+// import RenderProps2 from './hook_detail2/renderProps2';
+// import TestUseResize from './hook_detail2/useResize';
+import TestUseEffect from './hook_detail2/TestUseEffect';
+
+export default function HooksIndex() {
+  return (
+    <div>
+       {/* <StudyHook></StudyHook> */}
+      {/* <Button onClick={()=>{ setShow(!show) }}>show</Button>
+      {
+        show ? <TestUseDocTitle></TestUseDocTitle> : null
+      } */}
+      {/* <TestUseDocTitle></TestUseDocTitle> */}
+      {/* <TestUseEffect></TestUseEffect> */}
+      {/* <TestUseContext></TestUseContext> */}
+      {/* <TestUseState></TestUseState> */}
+      {/* <TestUseReducer></TestUseReducer> */}
+      {/* <TestUseCallback></TestUseCallback> */}
+      {/* <TestUseImperativeHandle></TestUseImperativeHandle> */}
+      {/* <TestMemo></TestMemo> */}
+      {/* <TestUseState4Child></TestUseState4Child> */}
+      {/* <Counter></Counter> */}
+      {/* <Counter2></Counter2> */}
+      {/* <Effect></Effect> */}
+      {/* <LayoutEffect></LayoutEffect> */}
+      {/* <ForwardRef></ForwardRef> */}
+      {/* <div>
+        <Counter1></Counter1>
+        <Counter2></Counter2>
+      </div> */}
+      {/* <CaptureValue></CaptureValue> */}
+      {/* <RenderProps></RenderProps> */}
+      {/* <RenderProps2></RenderProps2> */}
+      {/* <TestUseResize></TestUseResize> */}
+      <TestUseEffect></TestUseEffect>
+    </div>
+  )
+}
