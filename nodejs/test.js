25dd0de51da3ea34c5b9143186529bab626b4b6d
qiliang <chenqiliang@surveyplus.cn>
2021-12-28 11:29:26 +0800

    javascript同步任务微任务宏任务测试

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,4 +1,14 @@
 function testArgs(first, ...rest) {
     console.log("first is ", first, "rest is ", rest)
 }
-testArgs(1, 2, 3, 4)
+// testArgs(1, 2, 3, 4)
+setTimeout(() => { console.log(7) }, 0)
+console.log(1)
+const p = new Promise((resolve, reject) => {
+    console.log(2)
+    resolve()
+    console.log(3)
+})
+Promise.resolve().then(() => console.log(5))
+p.then(() => console.log(4))
+console.log(6)
