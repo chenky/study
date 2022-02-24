16303b77b85466cc3075489f1d2b644aa3140aa3
qiliang <chenqiliang@surveyplus.cn>
2022-02-24 11:41:05 +0800

    修复批量邀请脚本

--- nodejs/bossInvited/bossInvited3.js
+++ nodejs/bossInvited/bossInvited3.js
@@ -13,33 +13,56 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min //含最大值，含最小值 
 };
 
+function sleep(timeout) {
+    return new Promise(resolve => {
+        setTimeout(resolve, timeout)
+    })
+}
+
 /**
  * ".recommend-card-list>li button.btn-greet"
 */
 async function batchAdd(page, selector) {
     if (_count >= limitCount) return
-    let helloBtns = await page.$$(selector),
-        len = helloBtns.length
+    const helloBtns = await page.$$(selector)
+    const len = helloBtns.length
     console.log('helleBtns, len ', helloBtns, len)
-    let timeout = 0
-    for (let i = 0; i < len; i++) {
+    // let timeout = 0
+    // for (let i = 0; i < len; i++) {
+    //     timeout += getRandomIntInclusive(rndDown, rndUp) * 1000
+    //     console.log(`timeout is ${timeout}`)
+    //     setTimeout(function () {
+    //         _count++
+    //         if (_count >= limitCount) return
+    //         console.log(`helloBtns[i] is ${helloBtns[i]}`, ` _count is ${_count}`, ` len is ${len}`)
+    //         await helloBtns[i].click()
+    //         if (_count === len + 1) {
+    //             // 需要加载下一页了
+    //             const body = await page.$('body')
+    //             const { height } = await body.boundingBox()
+    //             console.log(`scrollTo bottom ${height}`)
+    //             await page.mouse.wheel({ deltaY: height })
+    //             await batchAdd()
+    //         }
+    //     }, timeout)
+    // }
 
-        timeout += getRandomIntInclusive(rndDown, rndUp) * 1000
+    for (const btn of helloBtns) {
+        const timeout = getRandomIntInclusive(rndDown, rndUp) * 1000
         console.log(`timeout is ${timeout}`)
-        setTimeout(function () {
-            _count++
-            if (_count >= limitCount) return
-            console.log(`helloBtns[i] is ${helloBtns[i]}`, ` _count is ${_count}`, ` len is ${len}`)
-            await helloBtns[i].click()
-            if (_count === len + 1) {
-                // 需要加载下一页了
-                const body = await page.$('body')
-                const { height } = await body.boundingBox()
-                console.log(`scrollTo bottom ${height}`)
-                await page.mouse.wheel({ deltaY: height })
-                await batchAdd()
-            }
-        }, timeout)
+        await sleep(timeout)
+        _count++
+        if (_count >= limitCount) return
+        console.log(`btn is ${btn}`, ` _count is ${_count}`)
+        await btn.click()
+        if (_count === len + 1) {
+            // 需要加载下一页了
+            const body = await page.$('body')
+            const { height } = await body.boundingBox()
+            console.log(`scrollTo bottom ${height}`)
+            await page.mouse.wheel({ deltaY: height })
+            await batchAdd()
+        }
     }
 
 }
@@ -48,11 +71,11 @@ async function batchAdd(page, selector) {
     const browser = await puppeteer.launch({
         defaultViewport: null,
         args: ["--window-position=0,0", "--window-size=1920,1040"],
-        executablePath:
-            "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        // executablePath:
+        //     "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
         // devtools: true,
         headless: false,
-        slowMo: 250, // slow down by 250ms
+        // slowMo: 250, // slow down by 250ms
     })
 
     const pages = await browser.pages()
